72d974eabfae91e09bee59204427bec321deef92
ArvinZJC <tomzjc@qq.com>
2022-04-23 21:09:28 +0800

    fix: the settings window's title bar does not follow the app theme change

--- PaimonTray/Views/GeneralSettingsPage.xaml.cs
+++ PaimonTray/Views/GeneralSettingsPage.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Windows.Storage;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using PaimonTray.Helpers;
+using Windows.Storage;
 
 namespace PaimonTray.Views
 {
--- PaimonTray/Views/SettingsWindow.xaml.cs
+++ PaimonTray/Views/SettingsWindow.xaml.cs
@@ -18,6 +18,12 @@ namespace PaimonTray.Views
     /// </summary>
     public sealed partial class SettingsWindow
     {
+        #region Fields
+
+        private AppWindow _appWindow;
+
+        #endregion Fields
+
         #region Constructors
 
         /// <summary>
@@ -33,70 +39,97 @@ namespace PaimonTray.Views
 
         #region Methods
 
+        // Customise the title bar.
+        private void CustomiseTitleBar()
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                _appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+                _appWindow.TitleBar.ButtonForegroundColor =
+                    (GridTitleBar.Resources["TitleBarCaptionForeground"] as SolidColorBrush)?.Color;
+                _appWindow.TitleBar.ButtonHoverBackgroundColor =
+                    (GridTitleBar.Resources["TitleBarButtonHoverBackground"] as SolidColorBrush)?.Color;
+                _appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                _appWindow.TitleBar.ButtonInactiveForegroundColor = GridTitleBar.ActualTheme switch
+                {
+                    ElementTheme.Dark => Colors.DimGray,
+                    ElementTheme.Default => null,
+                    ElementTheme.Light => Colors.Silver,
+                    _ => null
+                };
+                _appWindow.TitleBar.ButtonPressedBackgroundColor =
+                    (GridTitleBar.Resources["TitleBarButtonPressedBackground"] as SolidColorBrush)?.Color;
+                _appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+                GridColumnTitleBarLeftPadding.Width = new GridLength(_appWindow.TitleBar.LeftInset);
+                GridColumnTitleBarRightPadding.Width = new GridLength(_appWindow.TitleBar.RightInset);
+                GridTitleBar.Height = _appWindow.TitleBar.Height;
+            }
+            else
+            {
+                ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonBackgroundPointerOver"]).Color =
+                    ((SolidColorBrush)GridTitleBar.Resources["TitleBarButtonHoverBackground"]).Color;
+                ((SolidColorBrush)Application.Current.Resources["WindowCaptionForeground"]).Color =
+                    ((SolidColorBrush)GridTitleBar.Resources["TitleBarCaptionForeground"]).Color;
+                ((SolidColorBrush)Application.Current.Resources["WindowCaptionForegroundDisabled"]).Color =
+                    ((SolidColorBrush)GridTitleBar.Resources["TitleBarCaptionInactiveForeground"]).Color;
+                ExtendsContentIntoTitleBar = true;
+                SetTitleBar(GridTitleBar);
+            } // end if...else
+        } // end method CustomiseTitleBar
+
         // Customise the window.
         private async void CustomiseWindowAsync()
         {
             var windowId = WindowsHelper.GetWindowId(this);
-            var appWin = WindowsHelper.GetAppWindow(windowId);
 
+            _appWindow = WindowsHelper.GetAppWindow(windowId);
             Title = $"Settings - {Package.Current.DisplayName}";
 
-            if (appWin == null)
+            if (_appWindow == null)
             {
                 Log.Warning("The settings window's AppWindow is null.");
                 return;
             } // end if
 
-            appWin.SetIcon((await StorageFile.GetFileFromApplicationUriAsync(
-                new Uri(AppConstantsHelper.AppIconPath))).Path);
-
-            // Customise the title bar.
-            if (AppWindowTitleBar.IsCustomizationSupported())
-            {
-                appWin.TitleBar.ButtonBackgroundColor = Colors.Transparent;
-                appWin.TitleBar.ButtonHoverBackgroundColor =
-                    (Application.Current.Resources["WindowCaptionButtonBackgroundPointerOver"] as SolidColorBrush)
-                    ?.Color;
-                appWin.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
-                appWin.TitleBar.ExtendsContentIntoTitleBar = true;
-                GridColumnTitleBarLeftPadding.Width = new GridLength(appWin.TitleBar.LeftInset);
-                GridColumnTitleBarRightPadding.Width = new GridLength(appWin.TitleBar.RightInset);
-                GridTitleBar.Height = appWin.TitleBar.Height;
-            }
-            else
-            {
-                ExtendsContentIntoTitleBar = true;
-                SetTitleBar(GridTitleBar);
-            } // end if...else
+            _appWindow.SetIcon(
+                (await StorageFile.GetFileFromApplicationUriAsync(new Uri(AppConstantsHelper.AppIconPath)))
+                .Path);
+            CustomiseTitleBar();
 
             var workArea = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary).WorkArea;
 
-            appWin.Move(new PointInt32((workArea.Width - appWin.Size.Width) / 2,
-                (workArea.Height - appWin.Size.Height) / 2));
+            _appWindow.Move(new PointInt32((workArea.Width - _appWindow.Size.Width) / 2,
+                (workArea.Height - _appWindow.Size.Height) / 2));
         } // end method CustomiseWindowAsync
 
         #endregion Methods
 
         #region Event Handlers
 
-        // Handle the root navigation view's display mode changed event.
-        private void NavigationViewRoot_OnDisplayModeChanged(NavigationView sender,
+        // Handle the title bar grid's actual theme changed event.
+        private void GridTitleBar_OnActualThemeChanged(FrameworkElement sender, object args)
+        {
+            CustomiseTitleBar();
+        } // end method GridTitleBar_OnActualThemeChanged
+
+        // Handle the body navigation view's display mode changed event.
+        private void NavigationViewBody_OnDisplayModeChanged(NavigationView sender,
             NavigationViewDisplayModeChangedEventArgs args)
         {
-            NavigationViewRoot.IsPaneToggleButtonVisible = args.DisplayMode != NavigationViewDisplayMode.Expanded;
+            NavigationViewBody.IsPaneToggleButtonVisible = args.DisplayMode != NavigationViewDisplayMode.Expanded;
             ScrollViewerRoot.Padding = args.DisplayMode == NavigationViewDisplayMode.Minimal
                 ? new Thickness(18, 18, 18, 0)
                 : new Thickness(56, 18, 56, 0);
-        } // end method NavigationViewRoot_OnDisplayModeChanged
+        } // end method NavigationViewBody_OnDisplayModeChanged
 
-        // Handle the root navigation view's loaded event.
-        private void NavigationViewRoot_OnLoaded(object sender, RoutedEventArgs e)
+        // Handle the body navigation view's loaded event.
+        private void NavigationViewBody_OnLoaded(object sender, RoutedEventArgs e)
         {
             Activate();
-        } // end method NavigationViewRoot_OnLoaded
+        } // end method NavigationViewBody_OnLoaded
 
-        // Handle the root navigation view's selection changed event.
-        private void NavigationViewRoot_OnSelectionChanged(NavigationView sender,
+        // Handle the body navigation view's selection changed event.
+        private void NavigationViewBody_OnSelectionChanged(NavigationView sender,
             NavigationViewSelectionChangedEventArgs args)
         {
             Type pageType;
@@ -124,18 +157,18 @@ namespace PaimonTray.Views
             } // end switch-case
 
             FrameRoot.Navigate(pageType, null, new EntranceNavigationTransitionInfo());
-            NavigationViewRoot.Header = (args.SelectedItem as NavigationViewItem)?.Content;
-        } // end method NavigationViewRoot_OnSelectionChanged
+            NavigationViewBody.Header = (args.SelectedItem as NavigationViewItem)?.Content;
+        } // end method NavigationViewBody_OnSelectionChanged
 
-        // Handle the window's activated event.
+        // Handle the settings window's activated event.
         private void SettingsWindow_OnActivated(object sender, WindowActivatedEventArgs args)
         {
             if (args.WindowActivationState == WindowActivationState.Deactivated)
                 TextBlockWindowTitle.Foreground =
-                    Application.Current.Resources["WindowCaptionForegroundDisabled"] as SolidColorBrush;
+                    GridTitleBar.Resources["TitleBarCaptionInactiveForeground"] as SolidColorBrush;
             else
                 TextBlockWindowTitle.Foreground =
-                    Application.Current.Resources["WindowCaptionForeground"] as SolidColorBrush;
+                    GridTitleBar.Resources["TitleBarCaptionForeground"] as SolidColorBrush;
         } // end method SettingsWindow_OnActivated
 
         #endregion Event Handlers
